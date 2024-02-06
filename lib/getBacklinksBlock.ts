1c7737750199f8fa016a57e20680effadfa82b5f
John Turner <10634270+johndturn@users.noreply.github.com>
2024-02-05 21:33:26 -0700

    Working on tooling and getting it all to build

--- lib/getBacklinksBlock.ts
+++ lib/getBacklinksBlock.ts
@@ -1,15 +1,13 @@
-import * as MDAST from "mdast";
-import * as UNIST from "unist";
-import * as is from "unist-util-is";
+import type * as MDAST from 'mdast';
+import * as UNIST from 'unist';
+import * as is from 'unist-util-is';
 
 // Hacky type predicate here.
 function isClosingMatterNode(node: UNIST.Node): node is UNIST.Node {
-  return "value" in node && (node as MDAST.HTML).value.startsWith("<!--");
+  return 'value' in node && (node as MDAST.HTML).value.startsWith('<!--');
 }
 
-export default function getBacklinksBlock(
-  tree: MDAST.Root
-):
+export default function getBacklinksBlock(tree: MDAST.Root):
   | {
       isPresent: true;
       start: UNIST.Node;
@@ -22,27 +20,30 @@ export default function getBacklinksBlock(
   const existingBacklinksNodeIndex = tree.children.findIndex(
     (node: UNIST.Node): node is MDAST.Heading =>
       is(node, {
-        type: "heading",
-        depth: 2
-      }) && is((node as MDAST.Heading).children[0], { value: "Backlinks" })
+        type: 'heading',
+        depth: 2,
+      }) && is((node as MDAST.Heading).children[0], { value: 'Backlinks' }),
   );
+
   if (existingBacklinksNodeIndex === -1) {
     const insertionPoint =
-      tree.children.slice().reverse().find(node => is(node, isClosingMatterNode)) || null;
+      tree.children
+        .slice()
+        .reverse()
+        .find(node => is(node, isClosingMatterNode)) || null;
     return {
       isPresent: false,
-      insertionPoint
+      insertionPoint,
     };
   } else {
     const followingNode =
       tree.children
         .slice(existingBacklinksNodeIndex + 1)
-        .find(node => is(node, [{ type: "heading" }, isClosingMatterNode])) ||
-      null;
+        .find(node => is(node, [{ type: 'heading' }, isClosingMatterNode])) || null;
     return {
       isPresent: true,
       start: tree.children[existingBacklinksNodeIndex],
-      until: followingNode
+      until: followingNode,
     };
   }
 }

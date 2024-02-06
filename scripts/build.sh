04af83c3aef30c5e4c54c307ffabdaaf41e0b2b4
John Turner <10634270+johndturn@users.noreply.github.com>
2024-02-05 23:15:40 -0700

    Updated build to not push to the other repo

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,2 @@
 tsc -p . --outDir dist
 chmod +x dist/index.js
-rm -rf ~/Developer/Personal/personal-site/scripts/note-link-janitor/
-cp -r dist/ ~/Developer/Personal/personal-site/scripts/note-link-janitor/
